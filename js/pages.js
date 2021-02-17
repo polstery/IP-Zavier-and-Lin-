3f955cf188e0db9494fa690ada77f1e6feda62d3
polstery <s10206160@connect.np.edu.sg>
2021-02-18 00:27:42 +0800

    forgot2push -> layout index quiz done, js func wip

---
+++ js/pages.js
@@ -0,0 +1,27 @@
+//JS for all pages - Nav side bar, functionailty, buttons..etc
+$(document).ready(function () {
+
+    //toggle side bar
+    $("#menu-toggle").on("click", function (e) {
+        e.preventDefault();
+        $("#sidebar-nav").toggleClass("menuDisplayed");
+    })
+
+    //quizpage, toggle tab func.
+    $(".quizTabs").on("click", function (e) {
+        e.preventDefault();
+        $(".quizTabs").removeClass("active");
+    })
+
+    //quizpage, trigger forms for daily, norm and hard -> unneccessary (had the wrong bootstrap file downloaded)
+    $("#quiz-edu-btn").on("click", function (e) {
+        e.preventDefault();
+        $("#quiz-edu").toggleClass("formDisplayed");
+    })
+
+    $("#quiz-topic-btn").on("click", function (e) {
+        e.preventDefault();
+        $("#quiz-topic").toggleClass("formDisplayed");
+    })
+
+}); //End of doc
---
+++ js/quizFill.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    const APIKEY = "";
+
+    //retrieve Year / Stream input
+    yearList = []
+    for (let i = 0; i < document.form.quizEduYrForm.length; i++) {
+        if (document.form.quizEduYrForm[i].checked) {
+            console.log("EWW")
+        }
+    };
+
+    //display topics in the Topics form
+
+    //retrieve topic input
+
+    //validate submit (all other forms filled with at least 1 val)
+
+    //upload qns from relevant topics (yr and stream) into modal template
+
+    //function-object for modal template
+    function QuizQns() {
+        console.log("Hello YOU")
+    };
+
+}); //End of doc
